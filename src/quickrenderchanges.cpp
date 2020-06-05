cdbd1f0d0efe2781db23d6923cca2de000a01bba
ismo-karkkainen <3896372+ismo-karkkainen@users.noreply.github.com>
2020-06-05 22:32:17 +0300

    Quick render with partial unit tests.

--- src/quickrenderchanges.cpp
+++ src/quickrenderchanges.cpp
@@ -24,63 +24,129 @@
 #include <unistd.h>
 
 
-static void scale_changes(io::RenderChangesIn::changesType& Changes,
-    const std::uint32_t Size, const double MaxRadius)
-{
+struct ScaledChange {
+    double x, y, r;
+    std::int64_t c;
+    ScaledChange() : x(0.0), y(0.0), r(0.0), c(0) { }
+    ScaledChange(double X, double Y, double R, std::int64_t C) :
+        x(X), y(Y), r(R), c(C) { }
+};
+
+static double delta_range(const double V, const double Low, const double High) {
+    if (V < Low)
+        return Low - V;
+    if (High < V)
+        return V - High;
+    return 0.0;
+}
+
+static double max_abs_change(const io::RenderChangesIn::changesType& Changes) {
+    double maxabs = 0.0;
     for (auto& change : Changes) {
-        change[0] *= Size;
-        change[1] *= Size;
-        change[2] *= MaxRadius;
+        double cand = abs(change[3]);
+        if (maxabs < cand)
+            maxabs = cand;
+    }
+    return maxabs;
+}
+
+// Return value indicates if bounding box overlapped, i.e. close enough for
+// the placement in +/- Size to be well outside.
+static bool check_overlap(std::vector<ScaledChange>& Scaled,
+    const double X, const double Y, const double R, const std::int64_t D,
+    const double Left, const double Right, const double Low, const double High)
+{
+    // Check if bounding box of the circle intersects with target rectangle.
+    if (X + R < Left)
+        return false;
+    if (Right < X - R)
+        return false;
+    if (Y + R < Low)
+        return false;
+    if (High < Y - R)
+        return false;
+    // Check if circle intersects with target rectangle.
+    const double dx = delta_range(X, Left, Right);
+    const double dy = delta_range(Y, Low, High);
+    if (R * R < dx * dx + dy * dy)
+        return true;
+    Scaled.push_back(ScaledChange(X, Y, R, D));
+    return true;
+}
+
+static void scale_changes(std::vector<ScaledChange>& Scaled,
+    const io::RenderChangesIn::changesType& Changes, const double Size,
+    const double MaxRadius, const double ChangeScale,
+    const double Left, const double Right, const double Low, const double High)
+{
+    Scaled.resize(0);
+    for (auto& change : Changes) {
+        const double x = change[0] * Size;
+        const double y = change[1] * Size;
+        const double r = change[2] * MaxRadius;
+        const std::int64_t d =
+            static_cast<std::int64_t>(round(change[3] * ChangeScale));
+        check_overlap(Scaled, x, y, r, d, Left, Right, Low, High);
+        if (!check_overlap(Scaled, x - Size, y, r, d, Left, Right, Low, High))
+            check_overlap(Scaled, x + Size, y, r, d, Left, Right, Low, High);
+        if (!check_overlap(Scaled, x, y - Size, r, d, Left, Right, Low, High))
+            check_overlap(Scaled, x, y + Size, r, d, Left, Right, Low, High);
+        if (!check_overlap(Scaled, x - Size, y - Size, r, d, Left, Right, Low, High))
+            check_overlap(Scaled, x + Size, y + Size, r, d, Left, Right, Low, High);
+        if (!check_overlap(Scaled, x - Size, y + Size, r, d, Left, Right, Low, High))
+            check_overlap(Scaled, x + Size, y - Size, r, d, Left, Right, Low, High);
     }
 }
 
-static void row_deltas(const io::RenderChangesIn::changesType& Changes,
-    std::vector<double>& Deltas, std::uint32_t Y, const std::uint32_t Size,
-    const std::uint32_t Left, const std::uint32_t Right)
+static void row_deltas(std::vector<std::int64_t>& Deltas,
+    const std::vector<ScaledChange>& Scaled, const double Y,
+    const double Size, const double Left, const double Right)
 {
-    for (auto& change : Changes) {
-        const double cy = change[1];
-        const double radius = change[2];
-        double diff;
-        if (Y < cy) {
-            if (cy - Y < radius)
-                diff = cy - Y;
-            else if (Y + Size - cy < radius)
-                diff = Y + Size - cy;
-            else
-                continue;
-        } else {
-            if (Y - cy < radius)
-                diff = Y - cy;
-            else if (cy + Size - Y < radius)
-                diff = cy + Size - Y;
-            else
-                continue;
-        }
-        const double line_span = sqrt(radius * radius - diff * diff);
-        double from = round(change[0] - line_span);
-        double to = round(change[0] + line_span);
-        if (0 < from && Right < from) // On the right side of range.
+    for (auto& change : Scaled) {
+        double diff = abs(Y - change.y);
+        if (change.r < diff)
             continue;
-        if (to < Size && to < Left) // On the left side of range.
+        double line_span = sqrt(change.r * change.r - diff * diff);
+        double from = change.x - line_span;
+        if (from < 0.0)
+            from = 0.0;
+        else if (Right < from)
             continue;
-        // Wraps around?
-        if (from < 0)
-            from += Size;
-        else if (Size <= to)
-            to -= Size;
-        if (to < from) {
-            if (to < Left && Right < from)
-                continue;
-            Deltas.front() += change[3];
-            Deltas[to + 1] -= change[3];
-            Deltas[from] += change[3];
-        } else if (to == from) {
-            Deltas.front() += change[3];
-        } else {
-            Deltas[from] += change[3];
-            Deltas[to] -= change[3];
+        else {
+            // Check few adjacent values and pick smallest x that is within.
+            bool found = false;
+            for (double cx = floor(from) - 1.0;
+                cx < std::min(ceil(from) + 1.0, Size); ++cx)
+            {
+                double dx = cx - change.x;
+                if (dx * dx + diff * diff < change.r * change.r) {
+                    from = cx;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found || Size <= from)
+                continue; // Border-line case, no integer coordinate is inside.
         }
+        double to = change.x + line_span;
+        if (Size < to)
+            to = Size;
+        else if (to < Left)
+            continue;
+        else {
+            // Check few adjacent values and pick smallest x that is outside.
+            for (double cx = std::max(from + 1.0, floor(to) - 1.0);
+                cx < ceil(to) + 1.0; ++cx)
+            {
+                double dx = cx - change.x;
+                if (change.r * change.r < dx * dx + diff * diff) {
+                    to = cx;
+                    break;
+                }
+            }
+        }
+        Deltas[from] += change.c;
+        Deltas[to] -= change.c;
     }
 }
 
@@ -93,27 +159,37 @@ static void render_changes(io::RenderChangesIn& Val) {
     const std::uint32_t right = Val.rightGiven() ? std::min(Val.right(), size) : size;
     if (high <= low)
         return;
-    scale_changes(Val.changes(), size, 0.5 * Val.size());
+    const double max = max_abs_change(Val.changes());
+
+    std::int64_t change_room;
+    if ((1 << 12) < Val.changes().size())
+        change_room = 1LL << (63 - int(ceil(log2(Val.changes().size()))));
+    else
+        change_room = 1LL << 51;
+    const double change_scale = floor(static_cast<double>(change_room) / max);
+    std::vector<ScaledChange> scaled;
+    scale_changes(scaled, Val.changes(), size, 0.5 * Val.size(), change_scale,
+        left, right, low, high);
     std::vector<char> buffer;
-    std::vector<double> deltas(size + 1, 0.0);
+    std::vector<std::int64_t> deltas(size + 1, 0);
     std::vector<float> row;
     if (left < right)
         row.resize(right - left);
     for (std::uint32_t y = low; y < high; ++y) {
         if (left < right) {
-            row_deltas(Val.changes(), deltas, y, size, left, right);
-            double height = 0.0;
+            row_deltas(deltas, scaled, y, size, left, right);
+            std::int64_t height = 0;
             for (std::uint32_t n = 0; n < left; ++n) {
                 height += deltas[n];
-                deltas[n] = 0.0;
+                deltas[n] = 0;
             }
             for (std::uint32_t n = left; n < right; ++n) {
                 height += deltas[n];
-                deltas[n] = 0.0;
-                row[n - left] = height;
+                deltas[n] = 0;
+                row[n - left] = height / change_scale;
             }
             for (std::uint32_t n = right; n < deltas.size(); ++n)
-                deltas[n] = 0.0;
+                deltas[n] = 0;
         }
         io::Write(std::cout, row, buffer);
         if (y + 1 != high)
@@ -179,6 +255,95 @@ int main(int argc, char** argv) {
 
 #else
 
+TEST_CASE("delta_range") {
+    REQUIRE(delta_range(-1.0, 0.0, 2.0) == 1.0);
+    REQUIRE(delta_range(0.0, 0.0, 2.0) == 0.0);
+    REQUIRE(delta_range(1.0, 0.0, 2.0) == 0.0);
+    REQUIRE(delta_range(2.0, 0.0, 2.0) == 0.0);
+    REQUIRE(delta_range(3.0, 0.0, 2.0) == 1.0);
+}
+
+TEST_CASE("check_overlap") {
+    const double c = 1.0;
+    const double xl = 0.0;
+    const double xh = 2.0 * c;
+    const double yl = 0.0;
+    const double yh = 2.0 * c;
+    const std::int64_t cc = 1024;
+    std::vector<ScaledChange> scaled;
+    SUBCASE("inside") {
+        scaled.resize(0);
+        REQUIRE(check_overlap(scaled, c, c, c, cc, xl, xh, yl, yh));
+        REQUIRE(scaled.size() == 1);
+        REQUIRE(scaled.front().x == c);
+        REQUIRE(scaled.front().y == c);
+        REQUIRE(scaled.front().r == c);
+        REQUIRE(scaled.front().c == cc);
+    }
+    SUBCASE("left out") {
+        scaled.resize(0);
+        REQUIRE(!check_overlap(scaled, xl - 2.0 * c, c, c, cc, xl, xh, yl, yh));
+        REQUIRE(scaled.empty());
+    }
+    SUBCASE("left partial") {
+        scaled.resize(0);
+        const double x = xl - 0.5 * c;
+        REQUIRE(check_overlap(scaled, x, c, c, cc, xl, xh, yl, yh));
+        REQUIRE(scaled.size() == 1);
+        REQUIRE(scaled.front().x == x);
+        REQUIRE(scaled.front().y == c);
+        REQUIRE(scaled.front().r == c);
+        REQUIRE(scaled.front().c == cc);
+    }
+    SUBCASE("right partial") {
+        scaled.resize(0);
+        const double x = xh + 0.5 * c;
+        REQUIRE(check_overlap(scaled, x, c, c, cc, xl, xh, yl, yh));
+        REQUIRE(scaled.size() == 1);
+        REQUIRE(scaled.front().x == x);
+        REQUIRE(scaled.front().y == c);
+        REQUIRE(scaled.front().r == c);
+        REQUIRE(scaled.front().c == cc);
+    }
+    SUBCASE("right out") {
+        scaled.resize(0);
+        REQUIRE(!check_overlap(scaled, xh + 2.0 * c, c, c, cc, xl, xh, yl, yh));
+        REQUIRE(scaled.empty());
+    }
+    SUBCASE("bottom out") {
+        scaled.resize(0);
+        REQUIRE(!check_overlap(scaled, c, yl - 2.0 * c, c, cc, xl, xh, yl, yh));
+        REQUIRE(scaled.empty());
+    }
+    SUBCASE("left partial") {
+        scaled.resize(0);
+        const double y = yl - 0.5 * c;
+        REQUIRE(check_overlap(scaled, c, y, c, cc, xl, xh, yl, yh));
+        REQUIRE(scaled.size() == 1);
+        REQUIRE(scaled.front().x == c);
+        REQUIRE(scaled.front().y == y);
+        REQUIRE(scaled.front().r == c);
+        REQUIRE(scaled.front().c == cc);
+    }
+    SUBCASE("right partial") {
+        scaled.resize(0);
+        const double y = yh + 0.5 * c;
+        REQUIRE(check_overlap(scaled, c, y, c, cc, xl, xh, yl, yh));
+        REQUIRE(scaled.size() == 1);
+        REQUIRE(scaled.front().x == c);
+        REQUIRE(scaled.front().y == y);
+        REQUIRE(scaled.front().r == c);
+        REQUIRE(scaled.front().c == cc);
+    }
+    SUBCASE("top out") {
+        scaled.resize(0);
+        REQUIRE(!check_overlap(scaled, c, yh + 2.0 * c, c, cc, xl, xh, yl, yh));
+        REQUIRE(scaled.empty());
+    }
+}
+
+
+/*
 TEST_CASE("scale_changes") {
     io::RenderChangesIn::changesType changes(1);
     SUBCASE("Halves") {
@@ -315,5 +480,5 @@ TEST_CASE("row_deltas") {
         REQUIRE(deltas[4] == 0.0);
     }
 }
-
+*/
 #endif
