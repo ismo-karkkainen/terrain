1e7686a4fe94841370145ca75ad501cb0cfa950d
ismo-karkkainen <3896372+ismo-karkkainen@users.noreply.github.com>
2020-08-27 22:03:58 +0300

    Discard test build script dependency output for clones and builds.

--- test/port/Linux.sh
+++ test/port/Linux.sh
@@ -10,7 +10,7 @@ fi
 D=$(grep '^ID=' $F | sed 's/"/ /g' | sed 's/=/ /g' | sed 's/  / /g' | cut -d ' ' -f 2).sh
 
 if [ ! -x $1/test/port/$D ]; then
-    echo "Not supported:"
+    echo "Not supported, no matching script to value in ID-line:"
     cat /etc/os-release
     exit 1
 fi
--- test/port/centos.sh
+++ test/port/centos.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 set -eu
 sudo yum install -y -q cmake make clang ruby rake
-git clone --branch master --depth 1 https://github.com/onqtam/doctest.git dtroot
+git clone --branch master --depth 1 https://github.com/onqtam/doctest.git dtroot >/dev/null
 mv dtroot/doctest .
-git clone --branch master --depth 1 https://github.com/ismo-karkkainen/edicta.git
+git clone --branch master --depth 1 https://github.com/ismo-karkkainen/edicta.git >/dev/null
 cd edicta
-sudo rake install
+sudo rake install >/dev/null
 cd ..
-git clone --branch master --depth 1 https://github.com/ismo-karkkainen/specificjson.git
+git clone --branch master --depth 1 https://github.com/ismo-karkkainen/specificjson.git >/dev/null
 mkdir sjbuild
 cd sjbuild
 cp -r ../doctest .
-CXX=clang++ cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../specificjson
-make -j 3
-sudo make install
+CXX=clang++ cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../specificjson >/dev/null
+make -j 3 >/dev/null
+sudo make install >/dev/null
 cd ..
 CXX=clang++ cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release $1
 make -j 3
